e669f6396fa0d5f99f22e9b770d7a821bcc9d23c
Daniel Pérez <daniel.perez@ing.unlpam.edu.ar>
2023-09-06 16:42:34 -0300

    refactor: Mejora del código de re_examples.py

--- p1/re_examples.py
+++ p1/re_examples.py
@@ -1,14 +1,11 @@
 import re
 
-test_str = None
 with open("file.html", "r") as i:
-    test_str = i.readlines()
-test_str = "".join(test_str)
+    test_str = "".join(i.readlines())
+    regex = r"<tr>.*\n.*<td>(?P<Company>.*)</td>\n.*<td>(?P<Contact>.*)</td>.*\n.*<td>(?P<Country>.*)</td>"
+    matches = re.finditer(regex, test_str)
 
-regex = r"<tr>.*\n.*<td>(?P<Company>.*)</td>\n.*<td>(?P<Contact>.*)</td>.*\n.*<td>(?P<Country>.*)</td>"
-matches = re.finditer(regex, test_str)
-
-with open("output_file_path.csv", "w") as o:
-    for m in matches:
-        d = m.groupdict()
-        o.write(f"{d['Company']},{d['Contact']},{d['Country']}\n")
+    with open("output_file_path.csv", "w") as o:
+        for m in matches:
+            d = m.groupdict()
+            o.write(f"{d['Company']},{d['Contact']},{d['Country']}\n")
